20c7f99ab5c1c386b57425d3bae99504266e8a78
Judy <jzh24@cornell.edu>
2026-04-10 01:05:01 -0400

    Fixed deployment thingy

--- src/pages/Bathroom.tsx
+++ src/pages/Bathroom.tsx
@@ -1,8 +1,6 @@
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-const Bathroom = () => {
-    const{ id } = useParams()
-    
+const Bathroom = () => {    
     return (
         <div>
             <h1>The Baker Lab Basement Bathroom</h1>
