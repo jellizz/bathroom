dc07d1beafbc06d1c6acad5a8a89716e5d12c4e9
Judy <jzh24@cornell.edu>
2026-04-24 22:11:33 -0400

    login authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import LoginButton from './pages/Login'
 import Browse from './pages/Browse'
 import Add from './pages/Add'
 import BathroomDisplay from './pages/BathroomDisplay'
@@ -17,6 +18,7 @@ function App() {
         <Link to="/">Browse</Link> | 
         <Link to="/add">Add Bathroom</Link> |
         <span>Bathroom Map (not yet implemented)</span>
+        <LoginButton />
       </nav>
       <Routes>
         <Route path="/" element={<Browse />} /> 
